414b80b191ed9f7acac8675c22f71bb5fa11b4ad
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-23 13:48:59 -0800

    Adicionar definicao de selecionar opcao

--- js/features/criar_tipo_de_uso_Captacao_Em_Corpo_de_Agua.feature
+++ js/features/criar_tipo_de_uso_Captacao_Em_Corpo_de_Agua.feature
@@ -5,7 +5,9 @@ Funcionalidade: Formulário Modo de Uso Captação em Corpo de Água
    
   Cenario: Criação de um novo tipo
     Dado o sistema logado com o usuario "mbov" e com a senha "123"
-
+    Quando eu selecionar "CAPTAÇÃO EM CORPO DE ÁGUA (RIOS, LAGOAS NATURAIS ETC)" na listagem de "Tipo de uso"
+      E selecionar "Sim" no campo "Uso Insignificante"
+      
 
       
    
---
+++ js/features/step_definitions/selecionar_opcao.rb
@@ -0,0 +1,60 @@
+# language: pt
+# encoding: utf-8
+
+Quando /^eu selecionar "([^"]*)" na listagem de "([^"]*)"$/ do |opcao, nome_do_combobox|
+  selecionar_opcao_em_um_combobox( opcao, nome_do_combobox )
+end
+
+E /^selecionar "([^"]*)" na listagem de "([^"]*)"$/ do |opcao, nome_do_combobox|
+  selecionar_opcao_em_um_combobox( opcao, nome_do_combobox )
+end
+
+Quando /^eu selecionar "([^"]*)" no campo "([^"]*)"$/ do |opcao, nome_do_radiobutton|
+  selecionar_opcao_em_um_radio( opcao, nome_do_radiobutton )
+end
+
+E /^selecionar "([^"]*)" no campo "([^"]*)"$/ do |opcao, nome_do_radiobutton|
+  selecionar_opcao_em_um_radio( opcao, nome_do_radiobutton )
+end
+
+private
+
+  def selecionar_opcao_em_um_combobox opcao, campo
+    selecionar_opcao opcao, campo, 'combobox'
+  end
+
+  def selecionar_opcao_em_um_radio opcao, campo
+    selecionar_opcao opcao, campo, 'radio'
+  end
+
+  def selecionar_opcao opcao, campo, tipo_do_campo
+    selector_campo = create_selector_campo( campo )
+    element_campo = @driver.execute_script("return #{selector_campo}")
+    fail(ArgumentError.new('Campo nao encontrado!')) if element_campo.nil?
+
+    selector_opcao = create_selector_opcao( tipo_do_campo, selector_campo, opcao )
+    element_opcao = @driver.execute_script("return #{selector_opcao}")
+    fail(ArgumentError.new('Opcao nao encontrada!')) if element_opcao.nil?
+
+    element_opcao.click
+  end
+
+  def create_selector_campo campo
+    "$$(\"label:contains('#{campo}')\", \"span:contains('#{campo}')\")[0]"
+  end
+
+  def create_selector_opcao tipo_do_campo, selector_campo, opcao
+    case tipo_do_campo
+      when 'combobox' then create_selector_opcao_combobox( selector_campo, opcao )
+      when 'radio'    then create_selector_opcao_radio( selector_campo, opcao )
+    end
+  end
+
+  def create_selector_opcao_combobox selector_campo, opcao
+    "#{selector_campo}.getParent('tr').getElement(\"option:contains('#{opcao}')\")"
+  end
+
+  def create_selector_opcao_radio selector_campo, opcao
+    "#{selector_campo}.getParent('tr').getElement(\"span:contains('#{opcao}')\", \"label:contains('#{opcao}')\").previousElementSibling"
+  end
+

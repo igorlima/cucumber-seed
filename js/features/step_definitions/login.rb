062a9f50dc148caca1c31e44c9746afc9410f4aa
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-23 12:15:31 -0800

    Ajustar login

--- js/features/step_definitions/login.rb
+++ js/features/step_definitions/login.rb
@@ -9,5 +9,11 @@ Dado /^o sistema logado com o usuario "([a-zA-Z1-9]*)" e com a senha "([a-zA-Z1-
   input_username.send_keys username
   input_password.send_keys password
 
-  @driver.action.click btn_login
+  @driver.action.
+          click(btn_login).
+          perform
+
+  message_error = @driver.execute_script("return document.querySelector(\".message.error\")")
+  fail(ArgumentError.new('Usuario nao autenticado!')) if not message_error.nil?
+
 end

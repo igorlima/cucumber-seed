fc2affa9bb77ec749b8840a7aee3ed4a82d45c7c
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-24 10:40:30 -0800

    Preencher campo texto especificando o container

--- js/features/step_definitions/campo_texto.rb
+++ js/features/step_definitions/campo_texto.rb
@@ -9,16 +9,42 @@ E /^preencher "([^\"]*)" no campo "([^\"]*)"$/ do |texto, nome_campo|
   preencher_campo_texto( nome_campo, texto )
 end
 
+Quando /^eu preencher "([^\"]*)" no campo "([^\"]*)" em "([^\"]*)"$/ do |texto, nome_campo, container|
+  preencher_campo_texto_em_container( container, nome_campo, texto )
+end
+
+E /^preencher "([^\"]*)" no campo "([^\"]*)" em "([^\"]*)"$/ do |texto, nome_campo, container|
+  preencher_campo_texto_em_container( container, nome_campo, texto )
+end
+
 private
+
+  def preencher_campo_texto_em_container nome_container, nome_campo, texto
+    seletor = create_seletor_campo_texto_em_container nome_container, nome_campo
+    preencher_campo_texto_usando_seletor seletor, texto
+  end
 
   def preencher_campo_texto nome_campo, texto
     seletor = create_selector_campo_texto nome_campo
+    preencher_campo_texto_usando_seletor seletor, texto
+  end
+
+  def preencher_campo_texto_usando_seletor seletor, texto
     elemento_dom = @driver.execute_script("return #{seletor}")
     fail(ArgumentError.new('Campo texto nao encontrado!')) if elemento_dom.nil?
 
     elemento_dom.send_keys texto
   end
 
+  def seletor_campo_texto
+    ".getParent('tr').getElement(\"input[type='text']\")"
+  end
+
   def create_selector_campo_texto nome_campo
-    "#{create_selector_campo nome_campo}.getParent('tr').getElement(\"input[type='text']\")"
+    "#{create_selector_campo nome_campo}#{seletor_campo_texto}"
+  end
+
+  def create_seletor_campo_texto_em_container nome_container, nome_campo
+    seletor_container = create_selector_container nome_container
+    "#{seletor_container}.getElement(#{seletor_campo nome_campo})#{seletor_campo_texto}"
   end
--- js/features/step_definitions/elemento_dom.rb
+++ js/features/step_definitions/elemento_dom.rb
@@ -2,7 +2,14 @@
 # encoding: utf-8
 
 
+def seletor_campo nome_campo
+  "\"label:contains('#{nome_campo}')\", \"span:contains('#{nome_campo}')\""
+end
 
 def create_selector_campo campo
-  "$$(\"label:contains('#{campo}')\", \"span:contains('#{campo}')\")[0]"
+  "$$(#{seletor_campo campo})[0]"
+end
+
+def create_selector_container nome_container
+  "$$(\"div.header:contains('#{nome_container}')\")[0].getParent('table')"
 end

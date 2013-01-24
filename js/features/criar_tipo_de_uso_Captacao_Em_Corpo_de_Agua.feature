e2681a7fd53394b23e0fcc8759c995a3e8138a51
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-24 09:18:27 -0800

    Criar definicoes para clicar em um botao na tabela de caracterizacao

--- js/features/criar_tipo_de_uso_Captacao_Em_Corpo_de_Agua.feature
+++ js/features/criar_tipo_de_uso_Captacao_Em_Corpo_de_Agua.feature
@@ -15,7 +15,10 @@ Funcionalidade: Criar um novo modo de uso de Uso Captação em Corpo de Água
       E o Modo de Uso "CAPTAÇÃO EM CORPO DE ÁGUA (RIOS, LAGOAS NATURAIS ETC)" deve constar na segunda linha da tabela de caracterizacoes
       E o Modo de Uso "POÇO TUBULAR PROFUNDO" deve constar na primeira linha da tabela de caracterizacoes
 
-
+  Cenario: Preenchimento da primeira etapa
+    Dado o sistema logado com o usuario "mbov" e com a senha "123"
+    E clicar no botao "Editar" da primeira linha da tabela de caracterizacoes
+    
 
       
    
--- js/features/step_definitions/tabela_caracterizacoes.rb
+++ js/features/step_definitions/tabela_caracterizacoes.rb
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 E /^o Modo de Uso "([^\"]*)" deve constar na ([a-z]+) linha da tabela de caracterizacoes$/ do |modo_de_uso, ordem|
-	cardinal = converte_ordinal_em_cardinal ordem
+  cardinal = converte_ordinal_em_cardinal ordem
   verificar_modo_uso_na_tabela( modo_de_uso, cardinal )
 end
 
@@ -10,20 +10,43 @@ E /^o Modo de Uso "([^\"]*)" deve constar na linha ([0-9]+) da tabela de caracte
   verificar_modo_uso_na_tabela( modo_de_uso, ordem )
 end
 
+E /^clicar no botao "([^\"]*)" da ([a-z]+) linha da tabela de caracterizacoes$/ do |nome_do_botao, ordem|
+  cardinal = converte_ordinal_em_cardinal ordem
+  clicar_na_acao( nome_do_botao, cardinal )
+end
+
+E /^clicar no botao "([^\"]*)" da linha ([0-9]+) da tabela de caracterizacoes$/ do |nome_do_botao, ordem|
+  clicar_na_acao( nome_do_botao, ordem )
+end
+
 private
 
-	def verificar_modo_uso_na_tabela modo_de_uso, ordem
-		fail(ArgumentError.new("Nao EXISTE o modo de uso #{modo_de_uso} na linha #{ordem}")) unless existe_tal_modo_de_uso_na_linha_especificada? modo_de_uso, ordem
-	end
+  def clicar_na_acao nome_do_botao, ordem
+    selector = "#{selector_da_linha ordem}.getElement(#{create_selector_botao_da_tabela nome_do_botao})"
+    element_dom = @driver.execute_script("return #{selector}")
+    element_dom.click
+  end
 
-	def existe_tal_modo_de_uso_na_linha_especificada? modo_de_uso, ordem
-		ordem = ordem.to_i
-		selector = "$$(\"#tableAllForms tbody tr\")[#{ordem-1}].getElement(\":contains('#{modo_de_uso}')\")"
-		element_dom = @driver.execute_script("return #{selector}")
-		not element_dom.nil?
-	end
+  def verificar_modo_uso_na_tabela modo_de_uso, ordem
+    fail(ArgumentError.new("Nao EXISTE o modo de uso #{modo_de_uso} na linha #{ordem}")) unless existe_tal_modo_de_uso_na_linha_especificada? modo_de_uso, ordem
+  end
 
-	def converte_ordinal_em_cardinal ordinal
+  def existe_tal_modo_de_uso_na_linha_especificada? modo_de_uso, ordem
+    ordem = ordem.to_i
+    selector = "#{selector_da_linha ordem}.getElement(\":contains('#{modo_de_uso}')\")"
+    element_dom = @driver.execute_script("return #{selector}")
+    not element_dom.nil?
+  end
+
+  def selector_da_linha ordem
+    "$$(\"#tableAllForms tbody tr\")[#{ordem-1}]"
+  end
+
+  def create_selector_botao_da_tabela nome_do_botao
+    "\"a[title='#{nome_do_botao}']\""
+  end
+
+  def converte_ordinal_em_cardinal ordinal
     case ordinal
       when 'primeira' then 1
       when 'segunda'  then 2

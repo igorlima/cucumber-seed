ca39e19b9aaed507c8751ee1d8d4c434cf2fbd13
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-25 03:17:24 -0800

    Refatorar

--- js/features/step_definitions/campo_texto.rb
+++ js/features/step_definitions/campo_texto.rb
@@ -1,31 +1,31 @@
 # language: pt
 # encoding: utf-8
 
-E /^preencher "([^\"]*)" no campo "([^\"]*)"( caso esteja vazio)?$/ do |texto, nome_campo, is_vazio |
-  preencher_campo_texto( nome_campo, texto, is_vazio )
+E /^preencher "([^\"]*)" no campo "([^\"]*)"( caso esteja vazio)?$/ do |texto, nome_campo, caso_esteja_vazio |
+  preencher_campo_texto( nome_campo, texto, caso_esteja_vazio )
 end
 
-E /^preencher "([^\"]*)" no campo "([^\"]*)" em "([^\"]*)"( caso esteja vazio)?$/ do |texto, nome_campo, container, is_vazio |
-  preencher_campo_texto_em_container( container, nome_campo, texto, is_vazio )
+E /^preencher "([^\"]*)" no campo "([^\"]*)" em "([^\"]*)"( caso esteja vazio)?$/ do |texto, nome_campo, container, caso_esteja_vazio |
+  preencher_campo_texto_em_container( container, nome_campo, texto, caso_esteja_vazio )
 end
 
 private
 
-  def preencher_campo_texto_em_container nome_container, nome_campo, texto, is_vazio
+  def preencher_campo_texto_em_container nome_container, nome_campo, texto, caso_esteja_vazio
     seletor = create_seletor_campo_texto_em_container nome_container, nome_campo
-    preencher_campo_texto_usando_seletor seletor, texto, is_vazio
+    preencher_campo_texto_usando_seletor seletor, texto, caso_esteja_vazio
   end
 
-  def preencher_campo_texto nome_campo, texto, is_vazio
+  def preencher_campo_texto nome_campo, texto, caso_esteja_vazio
     seletor = create_selector_campo_texto nome_campo
-    preencher_campo_texto_usando_seletor seletor, texto, is_vazio
+    preencher_campo_texto_usando_seletor seletor, texto, caso_esteja_vazio
   end
 
-  def preencher_campo_texto_usando_seletor seletor, texto, is_vazio
+  def preencher_campo_texto_usando_seletor seletor, texto, caso_esteja_vazio
     elemento_dom = @driver.execute_script("return #{seletor}")
     fail(ArgumentError.new('Campo texto nao encontrado!')) if elemento_dom.nil?
 
-    if not is_vazio.nil?
+    if not caso_esteja_vazio.nil?
       text = @driver.execute_script("return #{seletor}.value")
       elemento_dom.send_keys texto if text.empty?
     else

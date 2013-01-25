3d282fd4f7957958477d6187be62ed10c6ed1484
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-25 10:24:58 -0800

    Implementar definicoes para verificar se um elemento DOM esta visivel. Refatorar identacao

--- js/features/step_definitions/elemento_dom.rb
+++ js/features/step_definitions/elemento_dom.rb
@@ -1,32 +1,41 @@
 # language: pt
 # encoding: utf-8
 
-
-def seletor_campo nome_campo
-  "\"label:contains('#{nome_campo}')\", \"span:contains('#{nome_campo}')\""
+E /^o campo "([^\"]*)" estiver visivel$/ do |campo|
+  fail(ArgumentError.new("O campo #{campo} nao esta visivel!")) unless componente_dom_visivel? campo
 end
 
-def create_selector_campo campo
-  "$$(#{seletor_campo campo})[0]"
+E /^o campo "([^\"]*)" estiver visivel em "([^\"]*)"$/ do |campo, container|
+  fail(ArgumentError.new("O campo #{campo} nao esta visivel em #{containes}")) unless componente_dom_visivel_em_container? container, campo
 end
 
-def create_selector_container nome_container
-  "$$(\"div.header:contains('#{nome_container}')\")[0].getParent('table')"
-end
+public
 
-def create_seletor_campo_em_container nome_container, nome_campo
-  seletor_container = create_selector_container nome_container
-  "#{seletor_container}.getElement(#{seletor_campo nome_campo})"
-end
+  def seletor_campo nome_campo
+    "\"label:contains('#{nome_campo}')\", \"span:contains('#{nome_campo}')\""
+  end
 
-def componente_dom_visivel? tipo_do_campo, campo, opcao
-  seletor = create_selector_campo( campo )
-  elemento_dom = @driver.execute_script("return #{seletor}")
-  elemento_dom.displayed?
-end
+  def create_selector_campo campo
+    "$$(#{seletor_campo campo})[0]"
+  end
 
-def componente_dom_visivel_em_container? container, tipo_do_campo, campo, opcao
-  seletor = create_seletor_campo_em_container container, campo
-  elemento_dom = @driver.execute_script("return #{seletor}")
-  elemento_dom.displayed?
-end
+  def create_selector_container nome_container
+    "$$(\"div.header:contains('#{nome_container}')\")[0].getParent('table')"
+  end
+
+  def create_seletor_campo_em_container nome_container, nome_campo
+    seletor_container = create_selector_container nome_container
+    "#{seletor_container}.getElement(#{seletor_campo nome_campo})"
+  end
+
+  def componente_dom_visivel? campo
+    seletor = create_selector_campo( campo )
+    elemento_dom = @driver.execute_script("return #{seletor}")
+    elemento_dom.displayed?
+  end
+
+  def componente_dom_visivel_em_container? container, campo
+    seletor = create_seletor_campo_em_container container, campo
+    elemento_dom = @driver.execute_script("return #{seletor}")
+    elemento_dom.displayed?
+  end

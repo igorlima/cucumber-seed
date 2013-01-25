5f8f943b54dcd185011769fb164ed23e05e53031
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-01-25 10:10:57 -0800

    Criar metodo que verifica se um elemento dom esta visivel no browser

--- js/features/step_definitions/elemento_dom.rb
+++ js/features/step_definitions/elemento_dom.rb
@@ -18,3 +18,15 @@ def create_seletor_campo_em_container nome_container, nome_campo
   seletor_container = create_selector_container nome_container
   "#{seletor_container}.getElement(#{seletor_campo nome_campo})"
 end
+
+def componente_dom_visivel? tipo_do_campo, campo, opcao
+  seletor = create_selector_campo( campo )
+  elemento_dom = @driver.execute_script("return #{seletor}")
+  elemento_dom.displayed?
+end
+
+def componente_dom_visivel_em_container? container, tipo_do_campo, campo, opcao
+  seletor = create_seletor_campo_em_container container, campo
+  elemento_dom = @driver.execute_script("return #{seletor}")
+  elemento_dom.displayed?
+end
